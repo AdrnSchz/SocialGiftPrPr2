95aa8c0e235e3024694513119d7d985d2e52f262
Alex Ciria Roca <alex.ciria@students.salle.url.edu>
2023-06-10 19:33:21 +0200

    initial screen, log in and sign up without apis done

--- app/src/main/java/com/example/socialgift/activities/InitialScreenActivity.java
+++ app/src/main/java/com/example/socialgift/activities/InitialScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.socialgift.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.socialgift.R;
 
@@ -12,5 +14,32 @@ public class InitialScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_screen);
+
+        Button signUpButton = findViewById(R.id.button_to_signup);
+        Button loginButton = findViewById(R.id.button_to_login);
+
+        signUpButton.setOnClickListener(v -> {
+            switchToSignUp();
+        });
+
+        loginButton.setOnClickListener(v -> {
+            switchToLogin();
+        });
+    }
+
+    private void switchToLogin() {
+        Intent gotoLogin = new Intent(this, LogInActivity.class);
+        gotoLogin.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        gotoLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(gotoLogin);
+        overridePendingTransition (0, 0);
+    }
+
+    private void switchToSignUp() {
+        Intent gotoSignup = new Intent(this, SignUpActivity.class);
+        gotoSignup.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        gotoSignup.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(gotoSignup);
+        overridePendingTransition (0, 0);
     }
 }
--- app/src/main/java/com/example/socialgift/activities/SignUpActivity.java
+++ app/src/main/java/com/example/socialgift/activities/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.socialgift.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.socialgift.R;
 
@@ -12,5 +15,22 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        EditText emailText = findViewById(R.id.email_signup);
+        EditText passwordText = findViewById(R.id.password_signup);
+        EditText confirmText = findViewById(R.id.confirm_signup);
+        Button button = findViewById(R.id.button_to_signup);
+
+        button.setOnClickListener(v -> {
+            switchToInitialScreen();
+        });
+    }
+
+    private void switchToInitialScreen() {
+        Intent gotoInitial = new Intent(this, InitialScreenActivity.class);
+        gotoInitial.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        gotoInitial.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(gotoInitial);
+        overridePendingTransition (0, 0);
     }
 }
