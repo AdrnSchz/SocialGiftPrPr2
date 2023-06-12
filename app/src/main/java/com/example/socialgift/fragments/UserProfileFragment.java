956947b8eacf9def07a1c61ad3d3d79b06bc28cc
adrianjorge.sanchez <adrianjorge.sanchez@students.salle.url.edu>
2023-06-12 20:35:05 +0200

    create gift implemented

--- app/src/main/java/com/example/socialgift/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/socialgift/fragments/UserProfileFragment.java
@@ -61,7 +61,6 @@ public class UserProfileFragment extends Fragment {
             Toast.makeText(getContext(), "Invalid URL", Toast.LENGTH_SHORT).show();
         }
 
-        //TODO: backButton
         backButton.setOnClickListener(v -> {
             thisActivity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view, new SearchFragment()).commit();
         });
